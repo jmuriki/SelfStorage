cfec3ab5a35b8d6e1804c206d2617e3530a86e1d
jiezzzzzzz <jiezzzzzzzzz@mail.ru>
2023-04-19 17:10:20 +0300

    added models

--- selfstorage/settings.py
+++ selfstorage/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "storage_rental",
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
--- storage_rental/admin.py
+++ storage_rental/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Cell)
+admin.site.register(Storage)
+admin.site.register(BookingRequest)
