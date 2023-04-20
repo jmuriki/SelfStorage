5d39e5517019505a91c23f38e345335cfb4c60c9
redbor24 <redbor24@gmail.com>
2023-04-20 12:40:12 +0300

    init version of QRCreate

--- storage_rental/models.py
+++ storage_rental/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import qrcode
+
 
 # Create your models here.
+
+def create_qr_code():
+    data = 'Мама мыла раму, сидя на подоконнике'  # self.get_order_num(self.id, self.user)
+    filename = f'{data}.png'
+    qrcode.make(data).save(filename)
+    return filename
