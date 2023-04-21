31c3799f0e5391e5f6ff4b7034d48aa0a17bbdde
jmuriki <jmuriki@mail.ru>
2023-04-21 13:03:58 +0530

    Update html templates

--- storage_rental/urls.py
+++ storage_rental/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('tariffs/', views.tariffs, name='tariffs'),
     path('calculator/', views.calculator, name='calculator'),
     path('rent_box/', views.rent_box, name='rent_box'),
-    path('can_be_stored/', views.can_be_stored, name='can_be_stored'),
+    path('faq/', views.faq, name='faq'),
     path('locations/', views.locations, name='locations'),
     path('contacts/', views.contacts, name='contacts'),
     path('customers_reviews/', views.customers_reviews, name='customers_reviews'),
@@ -15,5 +15,5 @@ urlpatterns = [
     path('sign_in/', views.sign_in, name='sign_in'),
     path('sign_up/', views.sign_up, name='sign_up'),
     path('restore/', views.restore, name='restore'),
-    # path('#/', views.#, name='#'),
+    path('account/', views.account, name='account'),
 ]
