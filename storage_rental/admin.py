afb08f48607b5009e96709ebef1866d5f8548295
redbor24 <redbor24@gmail.com>
2023-04-25 18:04:26 +0300

    Добавил Склад, Ячейку. Допилил Арендатора

--- storage_rental/admin.py
+++ storage_rental/admin.py
@@ -1,8 +1,26 @@
 from django.contrib import admin
 from .models import *
 
-# admin.site.register(Cell)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'email', 'phone_number']
+    search_fields = ['first_name', 'last_name']
+    fields = ['user', 'first_name', 'last_name', 'email', 'phone_number', 'avatar']
+
+
+class StorageAdmin(admin.ModelAdmin):
+    list_display = ['name', 'city', 'address', 'description', 'temperature', 'height']
+    fields = ['name', 'city', 'address', 'description', 'temperature', 'height']
+
+
+class CellAdmin(admin.ModelAdmin):
+    list_display = ['storage', 'cell_number', 'level', 'height', 'width', 'length', 'capacity', 'price']
+    fields = ['storage', 'cell_number', 'level', 'height', 'width', 'length', 'capacity', 'price']
+
+
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Storage, StorageAdmin)
+admin.site.register(Cell, CellAdmin)
 # admin.site.register(Storage)
 # admin.site.register(Order)
 # admin.site.register(Alert)
-admin.site.register(Customer)
--- storage_rental/models.py
+++ storage_rental/models.py
@@ -5,58 +5,157 @@ from django.contrib.auth.models import Permission, Group
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField(max_length=320, blank=True)
-    avatar = models.ImageField(upload_to='', blank=True)
-    name = models.CharField(max_length=250, blank=True)
-    surname = models.CharField(max_length=250, blank=True)
-    phone_number = PhoneNumberField(blank=True)
-    # bookings = models.ManyToManyField('Order', related_name='customers', blank=True)
-
-    groups = models.ManyToManyField(
-        'auth.Group',
-        verbose_name='groups',
-        blank=True,
-        related_name='storage_rental_users'
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь'
     )
-
-    user_permissions = models.ManyToManyField(
-        'auth.Permission',
-        verbose_name='user permissions',
+    email = models.EmailField(
+        max_length=320,
         blank=True,
-        related_name='storage_rental_users_permissions'
+        verbose_name='Адрес электронной почты'
+    )
+    avatar = models.ImageField(
+        upload_to='',
+        blank=True,
+        verbose_name='Аватарка'
+    )
+    first_name = models.CharField(
+        max_length=250,
+        blank=True,
+        verbose_name='Имя'
+    )
+    last_name = models.CharField(
+        max_length=250,
+        blank=True,
+        verbose_name='Фамилия'
+    )
+    phone_number = PhoneNumberField(
+        region='RU',
+        db_index=True,
+        blank=True,
+        verbose_name='Телефон'
     )
 
     def __str__(self):
-        return self.email
+        return f'{self.first_name} {self.last_name}, {self.email}'
+
+    class Meta:
+        verbose_name = 'Арендатор'
+        verbose_name_plural = 'Арендаторы'
 
 
-# class Cell(models.Model):
-#     cell_id = models.IntegerField(primary_key=True)
-#     capacity = models.FloatField()
-#     occupied = models.BooleanField(default=False)
-#     occupied_until = models.DateTimeField()
-#     price = models.CharField(max_length=250)
+class Storage(models.Model):
+    name = models.CharField(
+        max_length=255,
+        blank=False,
+        unique=True,
+        verbose_name='Наименование')
+    address = models.CharField(
+        max_length=1024,
+        blank=False,
+        unique=True,
+        verbose_name='Адрес')
+    city = models.CharField(
+        max_length=255,
+        blank=False,
+        verbose_name='Город'
+    )
+    description = models.CharField(
+        max_length=1024,
+        blank=False,
+        verbose_name='Описание')
+    temperature = models.FloatField(
+        null=False,
+        verbose_name='Температура хранения, гр. Цельсия'
+    )
+    height = models.FloatField(
+        null=False,
+        verbose_name='Высота потолка, м.'
+    )
 
+    def __str__(self):
+        return f'Склад "{self.name}", {self.city}, {self.address}, {self.description[:20]}'
+
+    class Meta:
+        verbose_name = 'Склад'
+        verbose_name_plural = 'Склады'
+
+
+class Cell(models.Model):
+    storage = models.ForeignKey(
+        Storage,
+        on_delete=models.CASCADE,
+        default=None,
+        related_name='storage',
+        verbose_name='Склад'
+    )
+    cell_number = models.CharField(
+        max_length=20,
+        blank=False,
+        verbose_name='Каталожный номер ячейки'
+    )
+    level = models.IntegerField(
+        null=False,
+        verbose_name='Этаж'
+    )
+    height = models.FloatField(
+        null=False,
+        verbose_name='Высота, м.'
+    )
+    width = models.FloatField(
+        null=False,
+        verbose_name='Ширина, м.'
+    )
+    length = models.FloatField(
+        null=False,
+        verbose_name='Длина, м.'
+    )
+    capacity = models.FloatField(
+        null=False,
+        verbose_name='Объём, м.куб.'
+    )
+    occupied = models.BooleanField(
+        default=False,
+        verbose_name='Занята'
+    )
+    price = models.FloatField(
+        null=False,
+        verbose_name='Стоимость хранения в ячейке'
+    )
+
+    def __str__(self):
+        return f'{self.storage.name}, {self.level} эт. №{self.cell_number}'
+
+    class Meta:
+        verbose_name = 'Ячейка'
+        verbose_name_plural = 'Ячейки'
+
+
+# class Order(models.Model):
+#     customer = models.ForeignKey(
+#         Customer,
+#         on_delete=models.CASCADE,
+#         related_name='customer',
+#         verbose_name='Заказчик'
+#     )
+#     cell = models.ManyToManyField(
+#         Cell,
+#         on_delete=models.CASCADE,
+#         related_name='cells',
+#         verbose_name='Ячейки'
+#     )
+#     date_from = models.DateTimeField('Дата начала хранения')
+#     date_to = models.DateTimeField('Дата окончания хранения')
+#     # Создан, Оплачен, Закрыт, Просрочен
+#     status = models.CharField(
+#         max_length=250,
+#         default='создан'
+#     )
+#
 #     class Meta:
-#         verbose_name_plural = "Ячейки"
-
-
-# class Storage(models.Model):
-#     storage_id = models.IntegerField(primary_key=True)
-#     name = models.CharField(max_length=250)
-#     city = models.CharField(max_length=250)
-#     address = models.CharField(max_length=1024)
-#     phone_number = PhoneNumberField(blank=True)
-#     email = models.EmailField(blank=True)
-#     count_cells = models.IntegerField()
-#     count_free_cells = models.IntegerField()
-#     temperature = models.FloatField()
-#     height = models.FloatField()
-#     cells = models.ForeignKey(Cell, on_delete=models.CASCADE, related_name='storage_cells')
-
-#     class Meta:
-#         verbose_name_plural = "Склады"
+#         verbose_name = 'Заказ'
+#         verbose_name_plural = 'Заказы'
 
 
 # class Image(models.Model):
@@ -67,18 +166,6 @@ class Customer(models.Model):
 #     class Meta:
 #         ordering = ['image_number']
 #         verbose_name_plural = "Картинки"
-
-
-# class Order(models.Model):
-#     user_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
-#     cell_id = models.ForeignKey(Cell, on_delete=models.CASCADE)
-#     start_booking = models.DateTimeField()
-#     end_booking = models.DateTimeField()
-#     status = models.CharField(max_length=250, default='создан')
-
-#     class Meta:
-#         unique_together = ['cell_id']
-#         verbose_name_plural = "Заказы"
 
 
 # class Alert(models.Model):
